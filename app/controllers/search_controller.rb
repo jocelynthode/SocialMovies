56b0fbdb5c010f9cb720ac634470dfa8abab5e0a
nad0u <ae.nadia@gmail.com>
2015-11-19 08:48:19 +0100

    testing sparql

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -12,5 +12,12 @@ class SearchController < ApplicationController
     query.execute(graph) do |solution|
       puts "name=#{solution.name} email=#{solution.email}"
     end
+
+    sparql = SPARQL::Client.new("http://data.linkedmdb.org/sparql")
+    query = sparql.select.where([:s, :p, :o]).offset(100).limit(10)
+
+    query.each_solution do |solution|
+      puts solution.inspect
+    end
   end
 end
