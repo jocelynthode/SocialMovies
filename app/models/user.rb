f491b846d041562358b320d10b4d96226d6502a3
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-11-27 10:21:38 +0100

    active recommendable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
-  # recommends :movies
+  recommends :movies
 end
