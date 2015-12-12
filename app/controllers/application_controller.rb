537cdf48c2c5803d3cc9ab3a362c34c934ddd40b
Simon Brulhart <simon@brulhart.me>
2015-12-12 19:07:34 +0100

    use redis to cache OMDB requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,15 +7,27 @@ class ApplicationController < ActionController::Base
   protected
 
   def omdb_query(imdb_id)
-    response = HTTP.get('http://www.omdbapi.com/', params: {
-        i: imdb_id,
-        plot: 'full',
-        r: 'json'
-    })
+    redis_key = 'omdb_' + imdb_id
+    response = redis.get(redis_key)
+    unless response
+      response = HTTP.get('http://www.omdbapi.com/', params: {
+          i: imdb_id,
+          plot: 'full',
+          r: 'json'
+      })
+      redis.set(redis_key, response.to_s)
+    end
+
     data = JSON.parse(response, symbolize_names: true)
     status = data[:Error] ? false : true
     [status, data]
   end
 
-
+  def redis
+    if @redis
+      @redis
+    else
+      @redis = Redis.new
+    end
+  end
 end
