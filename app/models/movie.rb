36f02d94a0a36aaa796a408377aa43ed96d0ee65
Simon Brulhart <simon@brulhart.me>
2015-12-12 17:41:52 +0100

    get IMDB id in Movie.retrieve

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,14 @@
 class Movie < ActiveRecord::Base
   has_and_belongs_to_many :lists
-  attr_accessor :title, :releaseDate, :actors
+  attr_accessor :title, :releaseDate, :actors, :imdb
 
   # TODO: put everything in one place
   RDF_PREFIXES = '
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
     PREFIX movie: <http://data.linkedmdb.org/resource/movie/>
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>'
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>'
   RDF_ENDPOINT = 'http://data.linkedmdb.org/sparql'
 
   # Retrieve movie model from datastore and add entry in local DB
@@ -15,12 +16,14 @@ class Movie < ActiveRecord::Base
     sparql = SPARQL::Client.new(RDF_ENDPOINT)
     q = %Q(
       #{RDF_PREFIXES}
-      SELECT ?id ?title ?releaseDate ?actorName
+      SELECT ?id ?title ?releaseDate ?imdb ?actorName
       WHERE {
         ?movies rdf:type movie:film .
         ?movies movie:filmid "#{mid}"^^xsd:int .
         ?movies rdfs:label ?title .
         ?movies movie:initial_release_date ?releaseDate .
+        ?movies foaf:page ?imdb .
+        FILTER regex(str(?imdb), '^http://www.imdb.com/title/')
         OPTIONAL {
           ?movies movie:filmid "#{mid}"^^xsd:int .
           ?movies movie:actor ?a .
@@ -35,6 +38,7 @@ class Movie < ActiveRecord::Base
     movie = Movie.find_or_create_by(id: mid)
     movie.title = res[0][:title].value
     movie.releaseDate = res[0][:releaseDate].value
+    movie.imdb = res[0][:imdb].value.split('/').last
     movie.actors = res.map{|x| x[:actorName]}.compact.map(&:value)
     movie
   end
@@ -46,6 +50,7 @@ class Movie < ActiveRecord::Base
     self.title = res.title
     self.releaseDate = res.releaseDate
     self.actors = res.actors
+    self.imdb = res.imdb
     self
   end
 end
