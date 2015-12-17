1dea03113be072ab992fc3c2af8963c7024711dd
Jocelyn Thode <jocelyn.thode@gmail.com>
2015-12-17 22:48:44 +0100

    fix new -> create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < Devise::RegistrationsController
   def create_default
     if resource.persisted?
       List.create({name: 'Watch It Later', user_id: @user.id})
-      List.new({name: 'Bookmarks', user_id: @user.id})
+      List.create({name: 'Bookmarks', user_id: @user.id})
     end
   end
 end
